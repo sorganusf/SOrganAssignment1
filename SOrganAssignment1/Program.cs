bdb9c43e1ae47c700aa4df1925d56df937fcfbf6
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-09 23:19:01 -0500

    Q6

--- SOrganAssignment1/Program.cs
+++ SOrganAssignment1/Program.cs
@@ -8,73 +8,28 @@ namespace SOrganAssignment1
 {
     class Program
     {
-        public static string UsfTime(string s)
+        static void Main(string[] args)
         {
-            int U1 = (int)s[0] - '0';
-            int U2 = (int)s[1] - '0';
-            int S1 = (int)s[3] - '0';
-            int S2 = (int)s[4] - '0';
-            int F1 = (int)s[6] - '0';
-            int F2 = (int)s[7] - '0';
-            int UU = (U1 * 10 + U2 % 10);
-            int SS = (S1 * 10 + S2 % 10);
-            int FF = (F1 * 10 + F2 % 10);
 
-            if (s[8] == 'A')
+            Console.Write("Input how many stones are in the bag: ");
+            string input = Console.ReadLine();
+            int numberOfStones = Convert.ToInt32(input);
+
+            if (numberOfStones % 4 == 0)
             {
-                int uCounter = 0;
-                int sCounter = 0;
-                int fCounter = 0;
-                int totalCount;
-                int timeChange = ((UU * 3600) + (SS * 60) + FF);
-                for (totalCount = 1; totalCount <= timeChange; totalCount++)
-                {
-                    if (fCounter <= 44)
-                    {
-                        fCounter++;
-                    }
-                    if (fCounter > 44)
-                    {
-                        sCounter++;
-                        fCounter = 0;
-                    }
-                }
-                Console.Write(uCounter + ":" + sCounter + ":" + fCounter);
+                Console.WriteLine("false");
+                Console.WriteLine("Explanation: As there are 4 stones" +
+                    " in the bag, you will never win the game.");
+                Console.WriteLine("No matter 1, 2, or 3 stones you take out," +
+                    " the last stone will always be removed by your friend.");
             }
             else
             {
-                UU = UU + 12;
-                int uCounter = 0;
-                int sCounter = 0;
-                int fCounter = 0;
-                int totalCount;
-                int timeChange = ((UU * 3600) + SS * 60) + FF;
-                for (totalCount = 0; totalCount < timeChange; totalCount++)
-                {
-                    if (fCounter <= 44)
-                    {
-                        fCounter++;
-                    }
-                    if (fCounter > 44)
-                    {
-                        sCounter++;
-                        fCounter = 0;
-                    }
-                    if (sCounter > 59)
-                    {
-                        uCounter++;
-                        sCounter = 0;
-                    }
-                }
-                Console.WriteLine(uCounter + ":" + sCounter + ":" + fCounter);
+                Console.WriteLine("Player 1 wins!");
+                Console.WriteLine("Player 1 will always win as long as they take the correct amount" +
+                    "of stones to prevent Player 2 from winning");
             }
-            return s;
-        }
-        public static void Main(string[] args)
-        {
-            string s = "09:15:35PM";
-            Console.WriteLine(s);
-            string t = UsfTime(s);
+
             Console.ReadLine();
         }
     }
