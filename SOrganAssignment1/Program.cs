ddb0a1a90885361636be57f695bc897e6c743307
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-09 23:24:13 -0500

    Q5

--- SOrganAssignment1/Program.cs
+++ SOrganAssignment1/Program.cs
@@ -8,28 +8,25 @@ namespace SOrganAssignment1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void PalindromePairs(string[] words)
         {
-
-            Console.Write("Input how many stones are in the bag: ");
-            string input = Console.ReadLine();
-            int numberOfStones = Convert.ToInt32(input);
-
-            if (numberOfStones % 4 == 0)
+            string checkWords = "";
+            string wordsReverse = "";
+            for (int i = 0; i < words.Length - 1; i++)
             {
-                Console.WriteLine("false");
-                Console.WriteLine("Explanation: As there are 4 stones" +
-                    " in the bag, you will never win the game.");
-                Console.WriteLine("No matter 1, 2, or 3 stones you take out," +
-                    " the last stone will always be removed by your friend.");
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    checkWords = words[i] + words[j];
+                    checkWords.Reverse();
+                    wordsReverse = words[j] + words[i];
+                }
             }
-            else
-            {
-                Console.WriteLine("Player 1 wins!");
-                Console.WriteLine("Player 1 will always win as long as they take the correct amount" +
-                    "of stones to prevent Player 2 from winning");
-            }
-
+            Console.Write("The palindromes are " + checkWords);
+        }
+        public static void Main(string[] args)
+        {
+            string[] words = new string[] { "abcd", "dcba", "lls", "s", "sssll" };
+            PalindromePairs(words);
             Console.ReadLine();
         }
     }
