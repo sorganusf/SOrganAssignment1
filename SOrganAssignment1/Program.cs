a18aeabc0e80bf92c1a8e6dd810bc37ffa3ab014
Sean O <54554261+sorganusf@users.noreply.github.com>
2020-02-09 23:49:21 -0500

    Update Program.cs
    Question 1 changes

--- SOrganAssignment1/Program.cs
+++ SOrganAssignment1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SOrganAssignment1
 {
@@ -10,10 +6,15 @@ namespace SOrganAssignment1
     {
         static void Main(string[] args)
         {
+            //Here I created nested for loops for each of the numbers.
+            //As each loop finishes, a number is subtracted off the next line.
             for (int z = 5; z >= 1; z--)
             {
+                //The nested loop is important in lessening each row by 1 number.
+                //54321 becomes 4321, then 321, etc.
                 for (int x = z; x > 0; x--)
                 {
+                    //I am printing each line as they progress through the loops.
                     Console.Write(x);
                 }
                 Console.WriteLine();
