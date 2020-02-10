9a9019893d77b27d81365e0f4aa0acdfa0c524b1
sorganusf <54554261+sorganusf@users.noreply.github.com>
2020-02-09 22:56:29 -0500

    Question1

---
+++ SOrganAssignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOrganAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                answer = i * (i + 1) / 2;
+
+                Console.Write(answer + ", ");
+            }
+        }
+    }
+}
