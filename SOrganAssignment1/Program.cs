0dd9b68b3baf47db1605fb3da1e6f7fc4492c2bc
Sean O <54554261+sorganusf@users.noreply.github.com>
2020-02-09 23:56:03 -0500

    Update Program.cs

--- SOrganAssignment1/Program.cs
+++ SOrganAssignment1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SOrganAssignment1
 {
@@ -10,16 +6,24 @@ namespace SOrganAssignment1
     {
         static void Main(string[] args)
         {
+            //Here I ask the user to enter a number.
+            //n will be the converted integer from the input.
             Console.Write("Enter a number: ");
             string input = Console.ReadLine();
             int n = Convert.ToInt32(input);
 
+            //Establishing answer variable.
             int answer = 0;
 
+            //Created for loop for each new number that
+            //will be created.
             for (int i = 1; i <= n; i++)
             {
+                //Answer becomes new number
                 answer = i * (i + 1) / 2;
 
+                //Number is printed each time through, being
+                //added on from the previous number in the cycle.
                 Console.Write(answer + ", ");
             }
             Console.ReadLine();
